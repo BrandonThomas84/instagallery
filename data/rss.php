5e8e084354834f7f83e09a8d55667d5213036621
Dave Bonillas <d_bonillas@dbonil0311814.build.internal>
2013-11-14 14:26:57 -0800

    cooliris wall

---
+++ data/rss.php
@@ -0,0 +1,27 @@
+<?php
+require_once(str_replace("/data","",dirname(__FILE__))."/lib/functions.php");
+$tag = $_GET['tag'];
+$mysql_query = 'SELECT * FROM hashdetail where hash = "'.$tag.'" and approved=1';
+
+$mysql_results = mysql_fetchAll($mysql_query);
+?>
+<?php echo '<?'; ?>xml version="1.0" encoding="utf-8" standalone="yes"<?php echo '?>'; ?>
+<rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+        <title>HELLO THERE</title>
+        <description>TAG WALL</description>
+        <link>http://local.instagallery.com/</link>
+        <?php foreach($mysql_results as $key => $result):
+                $imgSm = $result['image_thumb'];
+                $imgLg = $result['image_full'];
+            ?>
+            <item>
+                <title><?php echo htmlspecialchars($result['username']); ?></title>
+                <media:description><?php echo htmlspecialchars($result['text']); ?></media:description>
+                <link><?php echo $imgSm; ?></link>
+                <media:thumbnail url="<?php echo $imgSm; ?>"/>
+                <media:content url="<?php echo $imgLg; ?>"/>
+            </item>
+        <?php endforeach; ?>
+    </channel>
+</rss>
---
+++ wall.php
@@ -0,0 +1,45 @@
+<script type="text/javascript">
+
+    var cooliris = {
+        onEmbedInitialized : function() {
+            cooliris.embed.setCallbacks({
+                select: onItemSelected
+            });
+        }
+    };
+
+    function onItemSelected(item) {
+        if(item != null)
+        {
+            var slug = slugify(item.title);
+            cooliris.embed.deselectItem();
+            playerWindow(item.link,'725','350', 'playlist', slug );
+        }
+    }
+    function slugify(text) {
+        text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
+        text = text.replace(/-/gi, "_");
+        text = text.replace(/\s/gi, "-");
+
+        return (text + '').toLowerCase();
+    }
+</script>
+<html>
+<head>
+
+
+</head>
+<body>
+<div id="cooliris-wall">
+    <object id="ci_25733_o" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="607" height="375">
+        <param name="movie" value="http://apps.cooliris.com/embed/cooliris.swf"/>
+        <param name="allowFullScreen" value="true"/>
+        <param name="allowScriptAccess" value="always"/>
+        <param name="bgColor" value="#000000" />
+        <param name="flashvars" value="http://local.instagallery.com/data/rss.php?tag=sam_1234&amp;backgroundalpha=0.0&amp;numRows=3&amp;showEmbed=false&amp;showChrome=false&amp;showFullScreen=false" />
+        <param name="wmode" value="transparent" />
+        <embed id="ci_25733_e" type="application/x-shockwave-flash" src="http://apps.cooliris.com/embed/cooliris.swf" width="607" height="375" allowFullScreen="true" allowScriptAccess="always" bgColor="#000000" flashvars="feed=http://local.instagallery.com/data/rss.php?tag=sam_1234&amp;backgroundalpha=0.0&amp;numRows=3&amp;showEmbed=false&amp;showChrome=false&amp;showFullScreen=false" wmode="transparent"></embed>
+    </object>
+</div>
+</body>
+</html>
