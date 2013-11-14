7f5844d3c5e8bea7fe4f47f7cf5c8bf076d603bf
Sam <sam@build.com>
2013-11-13 16:50:16 -0800

    admin table started

--- admin/index.php
+++ admin/index.php
@@ -5,15 +5,22 @@ error_reporting(E_ALL);
 
 require_once("../lib/sql.php");
 
-
-echo "here";
-
 $query="SELECT id, hash, site_value, approved, image_full, image_thumb FROM hashdetail where approved=0;";
 $result = mysql_fetchAll($query);
 
+echo "<table border=1>";
+foreach ($result as $row)
+{
+   echo "<tr>";
+   echo "<td>".$row["id"]."</td>";
+   echo "<td>".$row["hash"]."</td>";
+   echo "<td>".$row["site_value"]."</td>";
+   echo "<td>".$row["image_thumb"]."</td>";
+   echo "</tr>";
+}
+echo "</table>";
 
-echo '<pre>'.print_r($result,true).'</pre>';
+//echo '<pre>'.print_r($result,true).'</pre>';
 
-echo "here";
 
 ?>
