3f570f4af402c020db4332b061a0c41c2988d342
Dave Bonillas <d_bonillas@dbonil0311814.build.internal>
2013-11-13 16:50:13 -0800

    More functionality.

--- cron/instaCron.php
+++ cron/instaCron.php
@@ -1,8 +1,9 @@
 <?php
 // Supply a user id and an access token
+require_once("../lib/sql.php");
 
 $clientId = 'c6f64314c0484c1382b26739a4502cd2';
-$tag = 'Kasi';
+$tag = 'SAM_1234';
 
 // Gets our data
 function fetchData($url){
@@ -15,15 +16,24 @@ function fetchData($url){
     return $result;
 }
 
+$mysql_query = 'SELECT * FROM hashtag';
+$mysql_results = mysql_fetchAll($mysql_query);
+
+echo '<pre>';
+print_r($mysql_results);
+echo '</pre>';
+
+foreach($mysql_results as $result){
 // Pulls and parses data.
-$query = fetchData("https://api.instagram.com/v1/tags/{$tag}/media/recent?client_id={$clientId}&count=0");
+   $tag = $result['hash']
+$insta_query = fetchData("https://api.instagram.com/v1/tags/{$tag}/media/recent?client_id={$clientId}&count=0");
 
-$result = json_decode($query);
+$result = json_decode($insta_query);
 
+
+foreach ($result->data as $post){
+
+    echo '<a class="group" rel="group1" href="' . $post->images->standard_resolution->url . '"><img src="'. $post->images->thumbnail->url .'"></a>';
+
+}
 ?>
-<?php foreach ($result->data as $post): ?>
-
-    <!-- Renders images. @Options (thumbnail,low_resoulution, high_resolution) -->
-    <a class="group" rel="group1" href="<?php echo $post->images->standard_resolution->url ?>"><img src="<?php echo $post->images->thumbnail->url ?>"></a>
-<?php endforeach ?>
-</html>
