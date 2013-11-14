c7587f43fb3f5490ca25a9f764b7dba9880026ca
Sam <sam@build.com>
2013-11-14 09:59:18 -0800

    fixing typoe

--- admin/index.php
+++ admin/index.php
@@ -62,7 +62,7 @@ $result = mysql_fetchAll($query);
 
 <?php
 
-			echo '<table class="table table-hover table-condensed table-bordered">';
+			echo '<table class="table table-hover table-condensed table-bordered" id="approvetable">';
 			echo '<thead>';
 			echo '<tr>';
 			echo '<th>Actions</th>';
@@ -76,11 +76,11 @@ $result = mysql_fetchAll($query);
 			foreach ($result as $row)
 			{
 			   echo '<tr id="ordrrow-'.$row["id"].'">';
-			   echo '<td> <a class="btn btn-danger block-photo" href="#" id="'.$row["id"].'">Block</a> <a class="btn btn-success approve-photo" href="#" id="'.$row["id"].'">Approve</a> </td>';
+			   echo '<td> <a class="btn btn-danger block-photo" href="" id="'.$row["id"].'">Block</a> <a class="btn btn-success approve-photo" href="" id="'.$row["id"].'">Approve</a> </td>';
 			   echo "<td>".$row["id"]."</td>";
 			   echo "<td>".$row["hash"]."</td>";
 			   echo "<td>".$row["site_value"]."</td>";
-			   echo '<td> <a href="#" rel="'.$row["image_full"].'" class="screenshot"><img src="'.$row["image_thumb"].'"></a></td>';
+			   echo '<td> <a rel="'.$row["image_full"].'" class="screenshot"><img src="'.$row["image_thumb"].'"></a></td>';
 
 			   echo "</tr>";
 			}
@@ -109,11 +109,11 @@ $('.block-photo').click(function(e) {
             $.ajax({
                 type: 'post',
                 url: site_url + 'admin/approvephoto.php',
-                data: {row_ id: id, task: 'block'},
+                data: {row_id: id, task: 'block'},
                 success: function(b) {
                     if (b) {
                         //delete row
-                        orTable.fnDeleteRow($('tr#ordrrow-' + id)[0]);
+                        $('tr#ordrrow-' + id).remove();
                     } else {
                         //failed to delete, sent noty in
                         noty({
@@ -143,7 +143,7 @@ $('.approve-photo').click(function(e) {
                 success: function(b) {
                     if (b) {
                         //delete row
-                        orTable.fnDeleteRow($('tr#ordrrow-' + id)[0]);
+                        $('tr#ordrrow-' + id).remove();
                     } else {
                         //failed to delete, sent noty in
                         noty({
