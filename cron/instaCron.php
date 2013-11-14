51c6d0172900adfec23f3c37f7ac76da83b1194c
Dave Bonillas <d_bonillas@dbonil0311814.build.internal>
2013-11-14 09:00:33 -0800

    New function to look to see  if we have an image and new cron features

--- cron/instaCron.php
+++ cron/instaCron.php
@@ -1,9 +1,9 @@
 <?php
 // Supply a user id and an access token
-require_once("../lib/sql.php");
+require_once("../lib/functions.php");
 
 $clientId = 'c6f64314c0484c1382b26739a4502cd2';
-$tag = 'SAM_1234';
+
 
 // Gets our data
 function fetchData($url){
@@ -19,21 +19,30 @@ function fetchData($url){
 $mysql_query = 'SELECT * FROM hashtag';
 $mysql_results = mysql_fetchAll($mysql_query);
 
-echo '<pre>';
-print_r($mysql_results);
-echo '</pre>';
 
 foreach($mysql_results as $result){
 // Pulls and parses data.
-   $tag = $result['hash']
-$insta_query = fetchData("https://api.instagram.com/v1/tags/{$tag}/media/recent?client_id={$clientId}&count=0");
+   $tag = $result['hash'];
+   $site_value = $result['site_value'];
+   $hash = $result['hash'];
 
-$result = json_decode($insta_query);
+    $insta_query = fetchData("https://api.instagram.com/v1/tags/{$tag}/media/recent?client_id={$clientId}&count=0");
 
+    $result = json_decode($insta_query);
 
-foreach ($result->data as $post){
+    foreach ($result->data as $post){
+        $full_img = $post->images->standard_resolution->url;
+        $thumb_img = $post->images->thumbnail->url;
+        $approved = 0;
 
-    echo '<a class="group" rel="group1" href="' . $post->images->standard_resolution->url . '"><img src="'. $post->images->thumbnail->url .'"></a>';
+        if(!findImg($full_img))
+        {
+            $insert_query = "INSERT INTO hashdetail (id, hash, site_value, approved, image_full, image_thumb) VALUES (NULL, '".$hash."', '".$site_value."', '0', '".$full_img."', '".$thumb_img."');";
+            mysql_insert($insert_query);
+        }
 
+    }
 }
-?>
+
+
+
