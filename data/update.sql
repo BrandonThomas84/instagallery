eb5460333d3af08e0dcd65b7fbf66a0a83e403da
Sam <sam@build.com>
2013-11-15 13:44:31 -0800

    more data updates

--- data/update.sql
+++ data/update.sql
@@ -1,5 +1,4 @@
-#ALTER TABLE instagallery.hashtag ADD origin VARCHAR(255);
 
-update hash set origin='http://staging.build.com/kohler-k-690/s563125' where hash='buildlangz';
-update hash set origin='http://staging.tapoutlet.co.uk/insinkerator-75275-stainless-steel-evolution-200-food-waste-disposer-75-hp-3-stage-grind-technology-air-switch.html' where hash='builddbemo';
-update hash set origin='http://staging.faucetdirect.com/kohler-k-596-simplice-pullout-spray-kitchen-faucet/p685721' where hash='buildsj5fr';
+update hashtag set origin='http://staging.build.com/kohler-k-690/s563125' where hash='buildlangz';
+update hashtag set origin='http://staging.tapoutlet.co.uk/insinkerator-75275-stainless-steel-evolution-200-food-waste-disposer-75-hp-3-stage-grind-technology-air-switch.html' where hash='builddbemo';
+update hashtag set origin='http://staging.faucetdirect.com/kohler-k-596-simplice-pullout-spray-kitchen-faucet/p685721' where hash='buildsj5fr';
